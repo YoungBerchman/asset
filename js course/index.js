8c51aa64ab79873d99f5e9fa3ddedfe16075f3b6
YoungBerchman <praiseegbe99@gmail.com>
2024-11-19 23:55:23 +0100

    html course

---
+++ js course/index.js
@@ -0,0 +1,12 @@
+const person = {
+    name: "prince",
+    height: 17,
+    status: "single",
+    state: "lagos",
+    profession: "software engineer",
+    sex: "male",
+    confirmSex:true,
+};
+
+const preciousStones = ["gold","pearl","onyx",500,true,undefined,null];
+const beforeMutateObject = person;
